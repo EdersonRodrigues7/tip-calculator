4740a09d4e4aedcbad6d0c9d201150a41f9a63c3
EdersonRodrigues7 <ederson@trilhante.com.br>
2021-07-20 21:15:08 -0300

    initial script

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const billValue = document.querySelector('#billValue').value;
-console.log(document.querySelector('#billValue').value);
-const tipPct = 15;
-const people = document.querySelector('#number').value;
-console.log(document.querySelector('#number').value);
+let tipPct = 0;
 
-const calcTip = bill => {
-  let tip = bill / tipPct;
-  console.log(`Tip per person: ${tip / people}`);
-  let total = (billValue + tip) / people;
-  console.log(`Total per person: ${total}`);
-  return tip, total;
-};
+document.querySelector('.button').addEventListener('click', function () {
+  tipPct = document.querySelector('.button').value;
+  console.log(tipPct);
+});
 
-calcTip(billValue);
+let people = 0;
+
+let billValue = 0;
+
+document.querySelector('#billValue').addEventListener('change', function () {
+  billValue = document.querySelector('#billValue').value;
+  people = document.querySelector('#number').value;
+  let tip = billValue * (tipPct / 100);
+
+  document.querySelector('#tip-amount').textContent = `$${tip / 5}`;
+  document.querySelector('#total').textContent = `$${billValue / 5 + tip / 5}`;
+
+  console.log(billValue, people);
+});
+
+document.querySelector('#number').addEventListener('change', function () {
+  people = document.querySelector('#number').value;
+});
